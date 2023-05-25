db5f06cee074995712ecd357b0d4994939dc3ad6
Pedro Saint Clair Garcia <pedro.clair@aluno.ufop.edu.br>
2023-05-25 15:46:46 -0300

    Portifolio 1.05 messages

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
+import sqlite3 as sql
 
 app = Flask(__name__)
 
@@ -34,5 +35,56 @@ def serve_image():
     message = "Image Route"
     return render_template('webPage/image.html', message=message)
 
+@app.route("/index")
+def index():
+    con=sql.connect("dataBase/db_web.db")
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur.execute("select * from users")
+    data=cur.fetchall()
+    return render_template("user/index_user.html",datas=data)
+
+@app.route("/add_user",methods=['POST','GET'])
+def add_user():
+    if request.method=='POST':
+        uname=request.form['uname']
+        contact=request.form['contact']
+        con=sql.connect("dataBase/db_web.db")
+        cur=con.cursor()
+        cur.execute("insert into users(UNAME,CONTACT) values (?,?)",(uname,contact))
+        con.commit()
+        flash('User Added','success')
+        return redirect(url_for("index"))
+    return render_template("user/add_user.html")
+
+@app.route("/edit_user/<string:uid>",methods=['POST','GET'])
+def edit_user(uid):
+    if request.method=='POST':
+        uname=request.form['uname']
+        contact=request.form['contact']
+        con=sql.connect("dataBase/db_web.db")
+        cur=con.cursor()
+        print("hello")
+        cur.execute("update users set UNAME=?,CONTACT=? where UID=?",(uname,contact,uid))
+        con.commit()
+        flash('User Updated','success')
+        return redirect(url_for("index"))
+    con=sql.connect("dataBase/db_web.db")
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur.execute("select * from users where UID=?",(uid,))
+    data=cur.fetchone()
+    return render_template("user/edit_user.html",datas=data)
+    
+@app.route("/delete_user/<string:uid>",methods=['GET'])
+def delete_user(uid):
+    con=sql.connect("dataBase/db_web.db")
+    cur=con.cursor()
+    cur.execute("delete from users where UID=?",(uid,))
+    con.commit()
+    flash('User Deleted','warning')
+    return redirect(url_for("index"))
+
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.secret_key='admin123'
+    app.run(debug=True)
